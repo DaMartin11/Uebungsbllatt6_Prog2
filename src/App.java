0c52a306dd6607aef6370bc6cb0606dc7573d552
DaMartin11 <martin.khd.74@gmail.com>
2022-11-16 23:35:04 +0100

    still working

--- src/App.java
+++ src/App.java
@@ -1,10 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("*******************************");
-        Point point =new Point(3,3);
+       /*  Point point =new Point(3,3);
         // point.distance(1,3);
          System.out.println("the distance: " + point.distance());
          System.out.println("the distance: " + point.distance(4, 0));
+        */
 
+        Point first = new Point(6,5);
+        Point second = new Point(3,1);
+        System.out.println("distance(0,0)= " + first.distance()); //7.810249675906654
+        System.out.println("distance(second)= " + first.distance(second)); // NaN u should fix it
+        System.out.println("distance(2,2)= " + first.distance(2,2));// 5.0
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());// 0.0
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -2,34 +2,62 @@
 public class Point{
     private int x;
     private int y;
-    public static void point(){
-        System.out.println("F");
+    public Point (){
+        
     }
-    public Point(int x, int y){
+    public Point (int x, int y){
         this.x = x; 
         this.y = y;
     }
-    public int getX(){
+
+    // Aufgabe 1.a
+
+    public int getX (){
         return x;
     }
-    public int getY(){
+
+    // Aufgabe 1.b
+
+    public int getY (){
         return y;
     }
-    public void setX(int x){
+
+    // Aufgabe 1.c
+
+    public void setX (int x){
         this.x = x;
     }
-    public void setY(int y){
+    
+    // Aufgabe 1.d
+
+    public void setY (int y){
         this.y = y;
     }
-    public double distance(){
+
+    // Aufgabe 1.e
+
+    public double distance (){
         double distance = Math.sqrt((Math.pow(x,2)) + Math.pow(y, 2)); 
         return distance;
-
     }
+
+    // Aufgabe 1.f
+
     public double distance (int x1,int y1){
          x1 = x1 - x;
          y1 = y1 - y;
         double distance = Math.sqrt((Math.pow(x1,2)) + Math.pow(y1, 2)); 
+        return distance;
+    }
+
+    // Aufgabe 1.g
+
+    public double distance (Point point){
+        int x1 = getX();
+        int y1 = getY();
+        int x2 = point.getX();
+        int y2 = point.getY();
+        double distance = Math.sqrt((Math.pow(x2, 2) - Math.pow(x1 , 2) + Math.pow(y2, 2) - Math.pow(y1,2)));
         return distance;
     }
 } 
